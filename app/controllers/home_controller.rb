ab2bc716b1b450bfe3f8f2b95cccf88c125c1330
Ben Austin <BenjaminBoydAustin@gmail.com>
2016-08-01 16:08:29 -0500

    Database loads correctly with api url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
 
+  get '/api' do
+    @legislators = Legislator.all
+    @legislators.to_json
+  end
+
   get '/?' do
     erb :home
   end
---
+++ db/migrate/20160801203438_vote.rb
@@ -0,0 +1,9 @@
+class Vote < ActiveRecord::Migration
+  def change
+    create_table :votes do |table|
+      table.string :person_id
+      table.string :name
+      table.string :eligible
+      table.string :missed
+  end
+end
