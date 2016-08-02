63ec5f21c3cfafd1cae77e1f1e03f67265238140
LeahNelson <webdevleah@gmail.com>
2016-08-02 17:19:05 -0500

    ajax call to govtrack api working

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -22,7 +22,28 @@ $('button').click(function(event){
   });
 });
 
+$('#pic').load(function() {
+  console.log("vote div loaded")
+
+  $.ajax({
+    url:'https://www.govtrack.us/api/v2/vote',
+    // https://www.govtrack.us/api/v2/vote/?congress=114&order_by=-created&limit=10
+    // ***** URL for 114th congress last 10 votes
+    type: 'get',
+    dataType: 'json',
+    // data: {
+    //   legislator: params[:govtrack_id]
+    // },
+    success: function(response) {
+      // bill = object[0].chamber;
+      console.log(response);
+      console.log(bill);
+    }
+  });
+
+});
 
 
-'<li><a href=' + response[i][2] + '>' + 
+
+// '<li><a href=' + response[i][2] + '>' + 
 // API Key: 787675d0ccec48fca8a936dc60ec27c6
