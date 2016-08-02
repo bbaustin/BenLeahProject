7b0b72d5962ddf59dc9414e03cb62aa5b2bf80bb
Ben Austin <BenjaminBoydAustin@gmail.com>
2016-08-02 12:00:09 -0500

    Results from state selection load on main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,12 +5,26 @@ class HomeController < ApplicationController
     @legislators.to_json
   end
 
+  get '/vote-api' do
+    @votes = Vote.all 
+    @votes.to_json
+  end
+
   get '/?' do
     erb :home
   end
 
   post '/?' do 
-
+    @leg_list = []
+    Legislator.all.each do |leg|
+      #puts leg['last_name']  <-- gives all last note. note to self -BA
+      if leg['state'] === params['state']
+         @leg_list.push(["#{leg['first_name']} #{leg['last_name']}", leg['state'], leg['govtrack_id']])
+      end
+    end
+    puts @leg_list
+    content_type :json 
+    @leg_list.to_json
   end
 
 end
--- app/public/js/main.js
+++ app/public/js/main.js
@@ -4,13 +4,17 @@ $('button').click(function(event){
   $.ajax({
     url: '/',
     type: 'post',
+    // dataType: 'json',
     data: { 
       state: $('select[name=state]').val() 
     },
     success: function(response) {
-      
-      console.log("success: " + response);
-
+      console.log(typeof response);
+      console.log(response);
+      $('p').html("");
+      for (var i=0; i <response.length; i++) {
+        $('p').append('<li>' + response[i][0] + '</li>');
+      }
     },
     error: function(error) {
       console.log("error: " + error);
