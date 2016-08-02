9e930294c8279910526e7b277ad083eb7948d308
LeahNelson <webdevleah@gmail.com>
2016-08-01 23:00:08 -0500

    write api call for photos

--- app/js/main.js
+++ app/js/main.js
@@ -24,3 +24,23 @@ $('button').click(function(event){
 
 // write ajax call using govttack_id to get photo and last 3 votes
 // Create learn more button on home.erb
+
+$('#learn-more').click(function(event) {
+  event.preventDefault();
+
+  var id = 
+
+  $.ajax({
+    url:'https://www.govtrack.us/data/photos/'+id+'-200px.jpeg',
+    type: 'get'
+    data: {
+
+    },
+    success: function(res) {
+
+    },
+    error: function(err){
+
+    }
+  });
+});
