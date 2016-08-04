7d7baaee6d9518d4dfd54e549333a254ca8131e0
Ben Austin <BenjaminBoydAustin@gmail.com>
2016-08-04 04:31:03 -0500

    Add flexbox, logic for ajax calls, social media icons

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,5 +1,4 @@
-// $('button').click(function(event){
-$('select').change(function(event){  
+$('select').change(function(event) {  
   event.preventDefault();
 
   $.ajax({
@@ -25,65 +24,31 @@ $('select').change(function(event){
 });
 // API Key: 787675d0ccec48fca8a936dc60ec27c6
 
-
-
-
-  // $.ajax({
-  //   url:'https://www.govtrack.us/api/v2/vote/?congress=114&order_by=-created&limit=10',
-  //   // https://www.govtrack.us/api/v2/vote
-  //   // ***** URL for 114th congress last 10 votes
-  //   type: 'get',
-  //   dataType: 'json',
-  //   // data: {
-  //   //   legislator: params[:govtrack_id]
-  //   // },
-  //   success: function(response) {
-  //     bill = response.objects[0].question;
-  //     link = response.objects[0].link;
-  //     console.log(response);
-  //     console.log(bill);
-  //     $('#question').append(bill);
-  //     $('#link').append(link);
-
-  //   },
-  //   error: function(error) {
-  //     console.log("error " + error );
-  //   }
-  // });
-
-  $.ajax({
-    url: 'https://www.govtrack.us/api/v2/committee_member',
-    type: 'get',
-    dataType: 'json',
-    success: function(response) {
-       params = window.location.href;
-       govtrack = parseInt(params.slice(-6));
-      
-      console.log(response);
-
-        var match = [];
-        for (var i = 0; i < response.objects.length; i++) {
-
-          if (govtrack == response.objects[i].person.id) {
-            var committee = response.objects[i].committee.name;
-            var role = response.objects[i].role;
-            var comRes = role + " of " + committee; 
-            $('.committee').append(comRes);
-            console.log(committee);
-            console.log(role);
-          } 
-          else 
-            console.log('no committee')        
-          }
-      // console.log(match);
-      // console.log(response);
-      // console.log(response.objects.length)
-    },
-    error: function(error) {
-      console.log("error " + error);
+$.ajax({
+  url: 'https://www.govtrack.us/api/v2/committee_member',
+  type: 'get',
+  dataType: 'json',
+  success: function(response) {
+    var params = window.location.href;
+    var govtrack = parseInt(params.slice(-6));      
+    var counter = 0; 
+    for (var i = 0; i < response.objects.length; i++) {
+      if (govtrack == response.objects[i].person.id) {
+        var committee = response.objects[i].committee.name;
+        var role = response.objects[i].role;
+        var comRes = role + " of " + committee; 
+        $('.committee').append(comRes);
+        counter++;
+      } 
     }
-
-  });
+    if (counter === 0) {
+      $('.committee').append('This government official is not currently a member of any congressional committees.');
+    }
+  },
+  error: function(error) {
+    console.log("error " + error);
+  }
+});
 
 
 $.ajax({
@@ -91,14 +56,19 @@ $.ajax({
   type: 'get',
   dataType: 'json',
   success: function(response) {
-    params = window.location.href;
-    govtrack = parseInt(params.slice(-6));
+    var params = window.location.href;
+    var govtrack = parseInt(params.slice(-6));
+    var counter = 0;
     for (var i = 0; i < response.objects.length; i++) { 
       if (govtrack == response.objects[i].sponsor.id) {
         $('.bill-title').append(response.objects[i].title);
         $('.bill-date').append(response.objects[i].introduced_date);
+        counter++;
         console.log(i);
       }
+    }
+    if (counter === 0) {
+      $('.bill-title').append('This government official has not sponsored any bills in 2016.');
     }
   },
   error: function(error) {
